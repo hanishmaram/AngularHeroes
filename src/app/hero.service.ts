0caf5262e060aec6a78e3026ba6c962854b46d7c
Hanish Maram <hmaram@jnetind.com>
2018-08-13 20:04:27 +0530

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HEROES} from './mock-heroes';
+import {Hero} from './hero';
+import {MessageService} from './message.service';
+
+import {Observable} from 'rxjs';
+import {of} from 'rxjs/observable/of';
+
+
+@Injectable()
+export class HeroService {
+
+  constructor(private messageService:MessageService) { }
+
+  // getTestHeroes():Observable<Hero[]>{
+  //   return HEROES;
+  // }
+
+  getHeroes():Observable<Hero[]>{
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+}
